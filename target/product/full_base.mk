cc148a4ddebaceac15fee68b01415e36d6ed6320
Peter Gregus <pete.gregus@gmail.com>
2013-01-29 21:37:18 +0100

    Remove VideoEditor and tts
    Change-Id: I57b39b98ee9e795fe7da7349a345c1a25f751d22

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -21,15 +21,7 @@
 
 PRODUCT_PACKAGES := \
     libfwdlockengine \
-    VideoEditor \
     WAPPushManager
-
-PRODUCT_PACKAGES += \
-    libvideoeditor_jni \
-    libvideoeditor_core \
-    libvideoeditor_osal \
-    libvideoeditor_videofilters \
-    libvideoeditorplayer
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
@@ -42,7 +34,7 @@ PRODUCT_LOCALES := en_US
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Get the TTS language packs
-$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
+#$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
